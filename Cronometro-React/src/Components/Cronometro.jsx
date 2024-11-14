0f2ccdf471e283cbe02d44623079cee96a304ee0
SarverryDanteAgustin <149842320+sarverryDante@users.noreply.github.com>
2024-11-14 15:56:22 -0300

    Update Cronometro.jsx

--- Cronometro-React/src/Components/Cronometro.jsx
+++ Cronometro-React/src/Components/Cronometro.jsx
@@ -29,12 +29,12 @@ function Cronometro() {
       <div className='w3-container w3-center'>
         <h1>{formatearTiempo()}</h1>
         <button className='w3-button w3-circle w3-black' onClick={() => (setCorriendo(prev => !prev))}>{corriendo ? 'Detener' : 'Iniciar'}</button>
-        {/*en linea 29: el prev toma el valor de corriendo y con '!' niega el estado, cambiandolo de true a false o viseversa.
+        {/*en linea 31: el prev toma el valor de corriendo y con '!' niega el estado, cambiandolo de true a false o viseversa.
         Tambien use un operador ternario ('?') para cambiar el texto dentro del button*/}
         <button className='w3-button w3-circle w3-yellow' onClick={() => (setCorriendo(false), setTiempo(0))}>Reiniciar</button>
-        {/*en linea 32: pone el corriendo en false detiendo el temporizador y el tiempo en 0*/}
+        {/*en linea 34: pone el corriendo en false detiendo el temporizador y el tiempo en 0*/}
         <button className='w3-button w3-circle w3-black' onClick={() => (setListaTiempos([...listaTiempos, formatearTiempo()]))}>Parcial</button>
-        {/*en linea 34: toma el tiempo actual de 'formatearTiempo' y lo agrega al '<ul>' dentro del componente 'ListaParcial'*/}
+        {/*en linea 36: toma el tiempo actual de 'formatearTiempo' y lo agrega al '<ul>' dentro del componente 'ListaParcial'*/}
         <h3>TIEMPOS</h3>
         <ListaParcial tiempos={listaTiempos} /> {/*mostramos el componente 'Lista Parcial'*/}
       </div>
